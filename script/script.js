bf88692c550e3ef7039db8673e95a60e80faa375
artem <bakirovartem69@gmail.com>
2022-07-14 15:51:26 +0200

    new one

---
+++ script/script.js
@@ -0,0 +1,16 @@
+let x = "Hallo";
+let y = "World<br>";
+
+let z = x + y;
+document.write(z);
+
+let n = x + " " + y;
+document.write(n);
+
+document.write(`${x} World`);
+
+let meinString = "Ich bin Erster";
+
+meinString += " Ich komme auf Platz zwei";
+
+console.log(meinString);
